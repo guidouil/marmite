fdae1f607ac143fe3896f4ac506b46debe5affd3
Guidouil <guidouil@gmail.com>
2015-10-23 10:22:46 +0200

    add fake product button

--- client/templates/comons/header/header_script.js
+++ client/templates/comons/header/header_script.js
@@ -9,6 +9,10 @@ Template.header.events({
   'click [data-action=goHome]': function () {
     Router.go('home');
   },
+  'click [data-action=addProduct]': function () {
+    Meteor.call('insertFakeProduct');
+    Router.go('list');
+  },
 });
 
 Template.header.onRendered(function ( ) {
